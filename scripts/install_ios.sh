37a8c21af432ba9bf0182a5b1e38f67789a3cd29
Jeremy Blalock <jeremyjblalock@gmail.com>
2020-03-26 15:49:28 -0500

    Updates scripts/install_ios.sh to work with react-native 0.61

--- scripts/install_ios.sh
+++ scripts/install_ios.sh
@@ -11,53 +11,28 @@ alias react-native="$(pwd)/node_modules/.bin/react-native"
 
 #yarn add react-native-maps -E #@^0.26.1
 yarn add react-native-maps@0.26.1
-react-native link react-native-maps
+# react-native link react-native-maps
 
 # Podfile
 cd ios
 
-pod init
+# cat <<EOF > /tmp/adalo-maps-podfile
+#
+# EOF
 
-echo "# Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+#sed -i.bak "$(cat /tmp/adalo-maps-podfile)" Podfile
+sed -i.bak '/use_native_modules/a\
+  pod "react-native-maps", path: "../node_modules/react-native-maps"\
+  pod "react-native-google-maps", path: "../node_modules/react-native-maps"  # Uncomment this line if you want to support GoogleMaps on iOS\
+  pod "GoogleMaps"  # Uncomment this line if you want to support GoogleMaps on iOS\
+  pod "Google-Maps-iOS-Utils" # Uncomment this line if you want to support GoogleMaps on iOS\
+' Podfile
 
-target '${name}' do
-  rn_path = '../node_modules/react-native'
-  rn_maps_path = '../node_modules/react-native-maps'
+# sed -i.bak '/usr_native_modules/a\
+#   pod "react-native-maps", path: "../node_modules/react-native-maps\"
+# ' Podfile
 
-  # See http://facebook.github.io/react-native/docs/integration-with-existing-apps.html#configuring-cocoapods-dependencies
-  pod 'yoga', path: \"#{rn_path}/ReactCommon/yoga/yoga.podspec\"
-  pod 'React', path: rn_path, subspecs: [
-    'Core',
-    'CxxBridge',
-    'DevSupport',
-    'RCTActionSheet',
-    'RCTAnimation',
-    'RCTGeolocation',
-    'RCTImage',
-    'RCTLinkingIOS',
-    'RCTNetwork',
-    'RCTSettings',
-    'RCTText',
-    'RCTVibration',
-    'RCTWebSocket',
-  ]
-
-  # React Native third party dependencies podspecs
-  pod 'DoubleConversion', :podspec => \"#{rn_path}/third-party-podspecs/DoubleConversion.podspec\"
-  pod 'glog', :podspec => \"#{rn_path}/third-party-podspecs/glog.podspec\"
-  # If you are using React Native <0.54, you will get the following error:
-  # Use the following line instead:
-  #pod 'GLog', :podspec => \"#{rn_path}/third-party-podspecs/GLog.podspec\"
-  pod 'Folly', :podspec => \"#{rn_path}/third-party-podspecs/Folly.podspec\"
-
-  # react-native-maps dependencies
-  pod 'react-native-maps', path: rn_maps_path
-  pod 'react-native-google-maps', path: rn_maps_path  # Uncomment this line if you want to support GoogleMaps on iOS
-  pod 'GoogleMaps'  # Uncomment this line if you want to support GoogleMaps on iOS
-  pod 'Google-Maps-iOS-Utils' # Uncomment this line if you want to support GoogleMaps on iOS
-end
-
+cat <<EOF >> Podfile
 post_install do |installer|
   installer.pods_project.targets.each do |target|
     if target.name == 'react-native-google-maps'
@@ -70,11 +45,12 @@ post_install do |installer|
         config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '8.0'
       end
     end
-    if target.name == \"React\"
+    if target.name == "React"
       target.remove_from_project
     end
   end
-end" >Podfile
+end
+EOF
 
 pod install
 
@@ -125,7 +101,7 @@ sed -i.bak '/@end/a\
 # Re-link react-native-maps
 
 cd ..
-react-native unlink react-native-maps
-react-native link react-native-maps
+# react-native unlink react-native-maps
+# react-native link react-native-maps
 
 echo "react-native-maps configuration completed for iOS."
