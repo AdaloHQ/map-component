7e76ce59abcf1453becf852492559614316c8039
Taylor Howard <46686409+TDH55@users.noreply.github.com>
2021-08-30 10:46:29 -0500

    points to production geocoder

--- src/Map/config.js
+++ src/Map/config.js
@@ -1,4 +1,3 @@
 export const markerWidth = 25
 export const markerHeight = 41
-export const geocodeURL = 'https://adalo-geocoder-staging.herokuapp.com/'
-//export const geocodeURL = "https://adalo-geocoder.herokuapp.com/"
+export const geocodeURL = "https://adalo-geocoder.herokuapp.com/"
