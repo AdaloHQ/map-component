a6592ed8e14238971aebdb37562a7217247070c1
Michael Aldridge <michael.aldridge@adalo.com>
2023-10-04 10:37:22 +0100

    amend android install script to be more specific

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -21,8 +21,8 @@ sed -i.bak '/targetSdkVersion/a\
 
 # insert meta-data tag before the closing </application> tag
 sed -i.bak '/<\/application>/i\
-    <meta-data android:name="com.google.android.geo.API_KEY" android:value="KEY" />\
+    <meta-data android:name="com.google.android.geo.API_KEY" android:value="GEO_API_KEY" />\
 ' app/src/main/AndroidManifest.xml
 
 # replace KEY with the actual key
-sed -i.bak "s/KEY/${key}/g" app/src/main/AndroidManifest.xml
+sed -i.bak "s/GEO_API_KEY/${key}/g" app/src/main/AndroidManifest.xml
