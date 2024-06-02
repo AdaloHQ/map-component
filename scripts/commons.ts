333d7b2d5a22b0a6d687b5529e6ac8a257c50ddd
Enzo Monjardín <enzomonjardin@users.noreply.github.com>
2024-06-02 18:14:27 -0300

    Update scripts to typescript and move old scripts to /legacy

---
+++ scripts/commons.ts
@@ -0,0 +1,47 @@
+import { join } from 'https://deno.land/std@0.224.0/path/mod.ts'
+
+export const backupFile = (filePath: string) =>
+  Deno.copyFile(filePath, `${filePath}.bak`)
+
+export const insertLineAfterString = (
+  multiLineText: string,
+  searchString: string,
+  lineToInsert: string,
+  options?: { insertBefore?: boolean }
+): string => {
+  const lines = multiLineText.split('\n')
+
+  let insertIndex = -1
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes(searchString)) {
+      insertIndex = i
+      break
+    }
+  }
+
+  if (insertIndex !== -1) {
+    const insertPosition = options?.insertBefore ? insertIndex : insertIndex + 1
+    lines.splice(insertPosition, 0, lineToInsert)
+  }
+
+  return lines.join('\n')
+}
+
+export const getMapsApiKey = async (projectPath: string) => {
+  try {
+    const protonBundlePath = join(projectPath, 'proton-bundle.json')
+    const protonBundleText = await Deno.readTextFile(protonBundlePath)
+    const protonBundle = JSON.parse(protonBundleText)
+    const apiKey =
+      protonBundle.libraryGlobals['@protonapp/map-component']['Map']['apiKey']
+
+    return apiKey
+  } catch (error) {
+    console.error(
+      'Error looking for maps api key in proton-bundle.json: ',
+      error.message
+    )
+    throw error
+  }
+}
