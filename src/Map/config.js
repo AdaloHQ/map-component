6d2cfb648cdda2d1d33c6c881377561231cbaf78
Daniel Cunha <danielcc.jp@gmail.com>
2024-11-29 10:16:08 -0300

    Fix/Geocoder URL and Upgrade React (#65)
    * Fix: Map | 2 different API keys for map causing crashing
    
    * fix: update geocoder url
    
    * deps: upgrade adalo cli
    
    * 0.5.46
    
    * 0.5.47
    
    * 0.5.48
    
    * 0.5.49
    
    * 0.5.50
    
    * deps: upgrade react
    
    ---------
    
    Co-authored-by: Michael Aldridge <michael.aldridge@adalo.com>

--- src/Map/config.js
+++ src/Map/config.js
@@ -1,4 +1,4 @@
 export const markerWidth = 25
 export const markerHeight = 41
-export const geocodeURL = "https://geocoder.adalo.com/"
+export const geocodeURL = 'https://geocoder.adalo.com/'
 export const defaultZoom = 13
